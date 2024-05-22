805f63c7043855d30f1c05b5b9139e8ab3c30e6c
kanishkasd <kanishkasandaru2048@gmail.com>
2024-05-22 15:22:27 +0530

    digitButton.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   DELETE_DIGIT: "delete-digit",
   CLEAR: "clear",
   CHOOSE_OPERATIONS: "choose-operation",
+  EVALUATE: "evaluate",
 };
 
-const reducer = (state, { type, payload }) => {};
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
+      };
+  }
+};
 
 function App() {
-  const [{ currentOperand, previousOperand, operation }] = useReducer(
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
     {}
   );
@@ -26,20 +36,20 @@ function App() {
 
       <button>DEL</button>
       <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
       <button>-</button>
       <button>.</button>
-      <button>0</button>
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
